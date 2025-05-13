4aaafbba4f90bb9df569d2376d805488e2d72b44
lnatamm <lnatamm25@gmail.com>
2025-05-13 01:36:39 -0300

    Implementando MLP

---
+++ trabalhos/Trabalho_Computacional_AV2/modelos_RNA/multi_layer_perceptron.py
@@ -0,0 +1,392 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class MLP:
+    def __init__(self, input_size, q, m, η=0.0001, ϵ=0.0001, tolleration=2, epochs=50):
+        """
+        input_size: Number of features in the input data
+        L: Número de camadas ocultas
+        q: Lista com a quantidade de neurônios em cada camada oculta
+        m: Número de neurônios na camada de saída
+        η: Taxa de aprendizagem (0 < η ≤ 1)
+        ϵ: Precisão do modelo (0 < ϵ ≤ 1)
+        tolleration: Número máximo de vezes que o modelo pode piorar antes de parar
+        """
+        self.__input_size = input_size
+        self.__q = q
+        self.__m = m
+        self.__η = η
+        self.__ϵ = ϵ
+        self.__tolleration = tolleration
+        self.__epochs = epochs
+
+        # Initialize weights and biases for each layer
+        self.__weights = []
+        # Inicializa os EQMs
+        self.__EQMs = []
+        # Histórico de pesos
+        self.__weights_history = []
+
+        # Lista com o tamanho de cada camada: entrada + ocultas + saída
+        layer_sizes = [self.__input_size] + self.__q + [self.__m]
+
+        for i in range(len(layer_sizes) - 1):
+            n_in = layer_sizes[i]
+            n_out = layer_sizes[i + 1]
+            # Inicializa pesos com valores entre -0.5 e 0.5 e inclui o bias (+1)
+            weight_matrix = np.random.random_sample((n_in + 1, n_out)) - 0.5
+            self.__weights.append(weight_matrix)
+        # Salva os pesos iniciais
+        self.__weights_history.append(self.__weights.copy())
+
+    # Função de ativação sigmoide logística
+    def __sigmoid_logistic(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    # Derivada da função de ativação sigmoide logística
+    def __sigmoid_logistic_derivative(self, x):
+        return x * (1 - x)
+
+    # Forward usado para tarefa de classificação
+    def forward_classification(self, x_sample):
+        """
+        Modelo de previsão para classificação.
+        x_sample: X de teste
+        """
+        self.__layer_inputs = []
+        self.__layer_outputs = []
+
+        for j, W in enumerate(self.__weights):
+            # Primeira camada: Camada de entrada
+            if j == 0:
+                # Combinação linear
+                layer_input = W.T @ x_sample
+                # Aplicação da função de ativação
+                layer_output = self.__sigmoid_logistic(layer_input)
+                self.__layer_inputs.append(layer_input)
+                self.__layer_outputs.append(layer_output)
+            # Camadas ocultas e de saída
+            else:
+                # Adição do bias à saída da camada anterior
+                y_bias = np.vstack([-1 * np.ones((1, self.__layer_outputs[-1].shape[1])), self.__layer_outputs[-1]])
+                # Combinação linear
+                layer_input = W.T @ y_bias
+                # Aplicação da função de ativação
+                layer_output = self.__sigmoid_logistic(layer_input)
+                self.__layer_inputs.append(layer_input)
+                self.__layer_outputs.append(layer_output)
+        # Retorna a saída da última camada
+        return self.__layer_outputs[-1].T
+
+    def forward_regression(self, x_sample):
+        """
+        Modelo de previsão para regressão.
+        x_sample: X de teste
+        """
+        self.__layer_inputs = []
+        self.__layer_outputs = []
+
+        for j, W in enumerate(self.__weights):
+            # Primeira camada: Camada de entrada
+            if j == 0:
+                # Combinação linear
+                layer_input = W.T @ x_sample
+                # Sem função de ativação, pois a tarefa é de regressão
+                layer_output = layer_input
+                self.__layer_inputs.append(layer_input)
+                self.__layer_outputs.append(layer_output)
+            # Camadas ocultas e de saída
+            else:
+                # Adição do bias à saída da camada anterior
+                y_bias = np.vstack([-1 * np.ones((1, self.__layer_outputs[-1].shape[1])), self.__layer_outputs[-1]])
+                # Combinação linear
+                layer_input = W.T @ y_bias
+                # Sem função de ativação, pois a tarefa é de regressão
+                layer_output = layer_input
+                self.__layer_inputs.append(layer_input)
+                self.__layer_outputs.append(layer_output)
+
+        return self.__layer_outputs[-1].T
+    
+    def __backward_regression(self, x_sample, d):
+        """
+        Algoritmo de backpropagation para atualizar pesos e biases.
+        x_sample: Amostra de entrada
+        d: Desejado
+        """
+        j = len(self.__weights) - 1
+        # Inicializa a lista de gradientes
+        δ = []
+        for i in range(len(self.__weights)):
+            δ.append([])
+        while j >= 0:
+            # Camada de saída
+            if j + 1 == len(self.__weights):
+                # Gradiente da função de ativação
+                δ[j] = self.__layer_inputs[j] * (d - self.__layer_outputs[j])
+                # adiciona o bias no y
+                y_bias = np.vstack([-1 * np.ones((1, self.__layer_outputs[j-1].shape[1])), self.__layer_outputs[j-1]])
+                # Atualiza os pesos
+                # Aplicamos o .T para que a soma de matrizes funcione
+                # Já que self.__weights[j] tem dimensões input_size x m e δ[j] @ y_bias.T tem dimensões m x input_size
+                self.__weights[j] += (self.__η * (δ[j] @ y_bias.T)).T
+            # Camada de entrada
+            elif j == 0:
+                # W[j+1] recebe a matriz W[j+1] sem a coluna que multiplica o bias
+                W_without_bias = self.__weights[j+1][1:, :]
+                # Gradiente da função de ativação
+                δ[j] = self.__layer_inputs[j] * W_without_bias @ δ[j+1]
+                # Atualiza os pesos
+                # Aplicamos o .T para que a soma de matrizes funcione
+                # Já que self.__weights[j] tem dimensões input_size x m e δ[j] @ y_bias.T tem dimensões m x input_size
+                self.__weights[j] += self.__η * (δ[j] @ x_sample.T).T
+            else:
+                # W[j+1] recebe a matriz W[j+1] sem a coluna que multiplica o bias
+                W_without_bias = self.__weights[j+1][1:, :]
+                # Gradiente da função de ativação
+                δ[j] = self.__layer_inputs[j] * W_without_bias @ δ[j+1]
+                # adiciona o bias no y
+                y_bias = np.vstack([-1 * np.ones((1, self.__layer_outputs[j-1].shape[1])), self.__layer_outputs[j-1]])
+                # Atualiza os pesos
+                # Aplicamos o .T para que a soma de matrizes funcione
+                # Já que self.__weights[j] tem dimensões input_size x m e δ[j] @ y_bias.T tem dimensões m x input_size
+                self.__weights[j] += self.__η * (δ[j] @ y_bias.T).T
+            j -= 1
+        # Salva os pesos após a atualização
+        self.__weights_history.append(self.__weights.copy())
+
+    def __backward_classification(self, x_sample, d):
+        """
+        Algoritmo de backpropagation para atualizar pesos e biases.
+        x_sample: Amostra de entrada
+        d: Desejado
+        """
+        j = len(self.__weights) - 1
+        # Inicializa a lista de gradientes
+        δ = []
+        for i in range(len(self.__weights)):
+            δ.append([])
+        while j >= 0:
+            # Camada de saída
+            if j + 1 == len(self.__weights):
+                # Gradiente da função de ativação
+                δ[j] = self.__sigmoid_logistic_derivative(self.__layer_inputs[j]) * (d - self.__layer_outputs[j])
+                # adiciona o bias no y
+                y_bias = np.vstack([-1 * np.ones((1, self.__layer_outputs[j-1].shape[1])), self.__layer_outputs[j-1]])
+                # Atualiza os pesos
+                # Aplicamos o .T para que a soma de matrizes funcione
+                # Já que self.__weights[j] tem dimensões input_size x m e δ[j] @ y_bias.T tem dimensões m x input_size
+                self.__weights[j] += (self.__η * (δ[j] @ y_bias.T)).T
+            # Camada de entrada
+            elif j == 0:
+                # W[j+1] recebe a matriz W[j+1] sem a coluna que multiplica o bias
+                W_without_bias = self.__weights[j+1][1:, :]
+                # Gradiente da função de ativação
+                δ[j] = self.__sigmoid_logistic_derivative(self.__layer_inputs[j]) * W_without_bias @ δ[j+1]
+                # Atualiza os pesos
+                # Aplicamos o .T para que a soma de matrizes funcione
+                # Já que self.__weights[j] tem dimensões input_size x m e δ[j] @ x_sample.T tem dimensões m x input_size
+                self.__weights[j] += self.__η * (δ[j] @ x_sample.T).T
+            else:
+                # W[j+1] recebe a matriz W[j+1] sem a coluna que multiplica o bias
+                W_without_bias = self.__weights[j+1][1:, :]
+                # Gradiente da função de ativação
+                δ[j] = self.__sigmoid_logistic_derivative(self.__layer_inputs[j]) * W_without_bias @ δ[j+1]
+                # adiciona o bias no y
+                y_bias = np.vstack([-1 * np.ones((1, self.__layer_outputs[j-1].shape[1])), self.__layer_outputs[j-1]])
+                # Atualiza os pesos
+                # Aplicamos o .T para que a soma de matrizes funcione
+                # Já que self.__weights[j] tem dimensões input_size x m e δ[j] @ y_bias.T tem dimensões m x input_size 
+                self.__weights[j] += self.__η * (δ[j] @ y_bias.T).T
+            j -= 1
+        # Salva os pesos após a atualização
+        self.__weights_history.append(self.__weights.copy())
+
+    def __EQM_classification(self, X, y):
+        """
+        Calcula o Erro Quadrático Médio (EQM).
+        X: Dados de entrada
+        y: Valores reais
+        """
+        EQM = 0
+        n_train_samples = X.shape[0]
+
+        for i in range(n_train_samples):
+            # N-ésima amostra de Xtreino
+            x_sample = X[i]
+            x_sample.shape = (x_sample.shape[0], 1)  # Reshape para garantir que seja uma matriz 2D
+
+            # Forward para obter as previsões para a amostra
+            y_pred = self.forward_classification(x_sample)
+
+            # N-ésimo rótulo de Xtreino
+            d = y[i]
+
+            # Calcula o erro quadrático individual (EQI)
+            EQI = 0
+            for j in range(self.__m):  # Para cada neurônio na camada de saída
+                EQI += (d[j] - y_pred[0][j]) ** 2
+
+            # Soma o EQI ao EQM
+            EQM += EQI
+
+        # Calcula o EQM final
+        EQM = EQM / (2 * n_train_samples)
+
+        return EQM
+
+    def __EQM_regression(self, X, y):
+        """
+        Calcula o Erro Quadrático Médio (EQM).
+        X: Dados de entrada
+        y: Valores reais
+        """
+        EQM = 0
+        n_train_samples = X.shape[0]
+
+        for i in range(n_train_samples):
+            # N-ésima amostra de Xtreino
+            x_sample = X[i]
+            x_sample.shape = (x_sample.shape[0], 1)  # Reshape para garantir que seja uma matriz 2D
+
+            # Forward para obter as previsões para a amostra
+            y_pred = self.forward_regression(x_sample)
+
+            # N-ésimo rótulo de Xtreino
+            d = y[i]
+
+            # Calcula o erro quadrático individual (EQI)
+            EQI = 0
+            for j in range(self.__m):  # Para cada neurônio na camada de saída
+                EQI += (d[j] - y_pred[0][j]) ** 2
+
+            # Soma o EQI ao EQM
+            EQM += EQI
+
+        # Calcula o EQM final
+        EQM = EQM / (2 * n_train_samples)
+
+        return EQM
+    
+    def __least_eqm(self, EQMs):
+        """
+        Retorna o índice do menor EQM
+        """
+        min_eqm = min(EQMs)
+        return EQMs.index(min_eqm)
+
+    def train_classification(self, X, y, X_validation, y_validation, epochs):
+        """
+        Treinamento do modelo para classificação.
+        X: Dados de entrada
+        y: Valores reais
+        X_validation: Dados de validação
+        y_validation: Valores reais de validação
+        epochs: Número de épocas para treinamento (Limitado pelo número máximo de épocas definido na inicialização)
+        """
+        # Adiciona o -1 como primeira coluna (bias)
+        X = np.hstack([-1 * np.ones((X.shape[0], 1)), X])
+        EQM = 1
+        # Salva os EQMs de validação
+        EQMs_validation = []
+        # Contador de tolerância
+        tolleration_count = 0
+        while True:
+            for epoch in range(epochs):
+                for i in range(X.shape[0]):  # Passa cada amostra individualmente
+                    x_sample = X[i]
+                    x_sample.shape = (x_sample.shape[0], 1)  # Reshape para garantir o shape correto
+                    y_sample = y[i]
+                    y_sample.shape = (y_sample.shape[0], 1)  # Reshape para garantir o shape correto
+                    output = self.forward_classification(x_sample)
+                    # Atualiza os pesos
+                    self.__backward_classification(x_sample, y_sample)
+                # Calcula o EQM
+                EQM = self.__EQM_classification(X, y)
+                print(f"Epoch {epoch}, Loss: {EQM:.4f}")
+                self.__EQMs.append(EQM)
+                
+                # Verifica se o modelo está piorando
+                if epoch > 1:
+                    # Faz a validação a cada 5 épocas
+                    if epoch % 5 == 0:
+                        print("Validando o modelo...")
+                        EQM_validation = self.__EQM_classification(X_validation, y_validation)
+                        print(f"Epoch {epoch}, EQM Validation: {EQM_validation:.4f}")
+                        EQMs_validation.append(EQM_validation)
+                        if len(EQMs_validation) > 1:
+                            if EQMs_validation[-1] > EQMs_validation[-2]:
+                                tolleration_count += 1
+                                print(f"Modelo piorou. Contagem de tolerância: {tolleration_count}/{self.__tolleration}")
+                                if tolleration_count >= self.__tolleration:
+                                    print(f"Early stopping ativado após {epoch} épocas devido à piora contínua.")
+                                    self.__weights = self.__weights_history[self.__least_eqm(self.__EQMs)]
+                            else:
+                                tolleration_count = 0  # Reseta a contagem se o modelo melhorar
+                    # Verifica se EQM atual é menor que a precisão do modelo
+                    if self.__EQMs[-1] < self.__ϵ:
+                        print(f"Convergiu após {epoch} épocas")
+                        return
+                
+                # Verifica se o número máximo de épocas foi atingido
+                if epoch >= self.__epochs:
+                    print(f"Número máximo de épocas atingido: {self.__epochs}")
+                    return
+    
+    def train_regression(self, X, y, X_validation, y_validation, epochs):
+        # Adiciona o -1 como primeira coluna (bias)
+        X = np.hstack([-1 * np.ones((X.shape[0], 1)), X])
+        X_validation = np.hstack([-1 * np.ones((X_validation.shape[0], 1)), X_validation])
+        EQM = 1
+        # Salva os EQMs de validação
+        EQMs_validation = []
+        # Contador de tolerância
+        tolleration_count = 0
+        while True:
+            for epoch in range(epochs):
+                for i in range(X.shape[0]):  # Passa cada amostra individualmente
+                    x_sample = X[i]
+                    x_sample.shape = (x_sample.shape[0], 1)  # Reshape para garantir o shape correto
+                    y_sample = y[i]
+                    y_sample.shape = (y_sample.shape[0], 1)  # Reshape para garantir o shape correto
+                    output = self.forward_regression(x_sample)
+                    # Atualiza os pesos
+                    self.__backward_regression(x_sample, y_sample)
+                # Calcula o EQM
+                EQM = self.__EQM_regression(X, y)
+                print(f"Epoch {epoch}, Loss: {EQM:.4f}")
+                self.__EQMs.append(EQM)
+                
+                # Verifica se o modelo está piorando
+                if epoch > 1:
+                    # Faz a validação a cada 5 épocas
+                    if epoch % 5 == 0:
+                        print("Validando o modelo...")
+                        EQM_validation = self.__EQM_regression(X_validation, y_validation)
+                        print(f"Epoch {epoch}, EQM Validation: {EQM_validation:.4f}")
+                        EQMs_validation.append(EQM_validation)
+                        if len(EQMs_validation) > 1:
+                            if EQMs_validation[-1] > EQMs_validation[-2]:
+                                tolleration_count += 1
+                                print(f"Modelo piorou. Contagem de tolerância: {tolleration_count}/{self.__tolleration}")
+                                if tolleration_count >= self.__tolleration:
+                                    print(f"Early stopping ativado após {epoch} épocas devido à piora contínua.")
+                                    self.__weights = self.__weights_history[self.__least_eqm(self.__EQMs)]
+                            else:
+                                tolleration_count = 0  # Reseta a contagem se o modelo melhorar
+                    # Verifica se EQM atual é menor que a precisão do modelo
+                    if self.__EQMs[-1] < self.__ϵ:
+                        print(f"Convergiu após {epoch} épocas")
+                        return
+                
+                # Verifica se o número máximo de épocas foi atingido
+                if epoch >= self.__epochs:
+                    print(f"Número máximo de épocas atingido: {self.__epochs}")
+                    return
+
+    def predict_classification(self, X):
+        return self.forward_classification(X)
+
+    def predict_regression(self, X):
+        # Retorna a combinação linear
+        return self.forward_regression(X)
--- trabalhos/Trabalho_Computacional_AV2/regressao.py
+++ trabalhos/Trabalho_Computacional_AV2/regressao.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from modelos_RNA.adaline import Adaline
+from modelos_RNA.multi_layer_perceptron import MLP
 from config import Config
 
 plot_graph = True
@@ -10,9 +11,6 @@ plot_graph = True
 aerogerador = np.loadtxt("trabalhos\\Trabalho_Computacional_AV2\\datasets\\aerogerador.dat")
 
 # Regressão Linear Simples
-
-# Instanciação os modelo
-adaline = Adaline(η=0.0001, ϵ=0.0001, epochs=1000)
 
 # Separação da variável dependente e independente
 X = aerogerador[:, 0]  # Variável independente (primeira coluna)
@@ -26,8 +24,10 @@ if Config.REGRESSION_PLOT_GRAPH:
     plt.scatter(X, y, color='blue', label='Dados Originais')
 
 # Normalização dos dados
-X = (X - np.mean(X)) / np.std(X)
-y = (y - np.mean(y)) / np.std(y)
+# X = (X - np.mean(X)) / np.std(X)
+X = (X - np.min(X)) / (np.max(X) - np.min(X))
+# y = (y - np.mean(y)) / np.std(y)
+y = (y - np.min(y)) / (np.max(y) - np.min(y))
 if Config.REGRESSION_PLOT_GRAPH:
     plt.figure(2)
     plt.title('Dados Normalizados')
@@ -35,8 +35,14 @@ if Config.REGRESSION_PLOT_GRAPH:
     plt.ylabel('Potência (kW)')
     plt.scatter(X, y, color='red', label='Dados Normalizados')
 
+# Instanciação os modelo
+adaline = Adaline(η=0.0001, ϵ=0.0001, epochs=1000)
+mlp = MLP(input_size=1, q=[2, 1], m=1, η=0.0001, ϵ=0.0001, tolleration=10, epochs=Config.REGRESSION_EPOCHS)
+
 eqms_adaline = []
 r_s_adaline = []
+eqms_mlp = []
+r_s_mlp = []
 
 for round in range(Config.REGRESSION_N_ROUNDS):
     print(f"Rodada: {round + 1}")
@@ -44,15 +50,18 @@ for round in range(Config.REGRESSION_N_ROUNDS):
     index = np.random.permutation(aerogerador.shape[0])
     X_shuffled = X[index]
     y_shuffled = y[index]
-    # Separação dos dados em treino e teste
+    # Separação dos dados em treino, teste e validação
     train_size = int(Config.REGRESSION_TRAIN * len(X))
+    validation_size = int(Config.REGRESSION_VALIDATION * len(X))
     # 80% para treino
     X_train = X_shuffled[:train_size]
     X_test = X_shuffled[train_size:]
     # 20% para teste
     y_train = y_shuffled[:train_size]
     y_test = y_shuffled[train_size:]
-
+    # 10% para validação
+    X_validation = X_test[:validation_size]
+    y_validation = y_test[:validation_size]
     # Etapa de treinamento
     # Adaline
     adaline.fit(X_train.reshape(-1, 1), y_train)
@@ -71,6 +80,20 @@ for round in range(Config.REGRESSION_N_ROUNDS):
     eqms_adaline.append(EQM_adaline)
     r_s_adaline.append(r2_adaline)
 
+    # Multi-Layer Perceptron
+    mlp.train_regression(X_train.reshape(-1, 1), y_train.reshape(-1, 1), X_validation.reshape(-1, 1), y_validation.reshape(-1, 1))
+    predictions_mlp = []
+    for x in X_test:
+        x = np.vstack((-1, x))
+        predictions_mlp.append(mlp.predict_regression(x.reshape(-1, 1)))
+    EQM_mlp = np.mean((y_test - predictions_mlp) ** 2)
+    ss_res_mlp = np.sum((y_test - predictions_mlp) ** 2)              # Soma dos quadrados dos resíduos
+    ss_tot_mlp = np.sum((y_test - np.mean(y_test)) ** 2)          # Soma total dos quadrados
+    r2_mlp = 1 - (ss_res_mlp / ss_tot_mlp)
+    print(f"EQM: {EQM_mlp:.4f}")
+    print(f"R²: {r2_mlp:.4f}")
+    eqms_mlp.append(EQM_mlp)
+    r_s_mlp.append(r2_mlp)
 metrics_adaline = {
     "mean_eqm": np.mean(eqms_adaline),
     "std_eqm": np.std(eqms_adaline),
@@ -82,42 +105,61 @@ metrics_adaline = {
     "min_r2": np.min(r_s_adaline),
     "std_eqm": np.std(eqms_adaline),
 }
+metrics_mlp = {
+    "mean_eqm": np.mean(eqms_mlp),
+    "std_eqm": np.std(eqms_mlp),
+    "max_eqm": np.max(eqms_mlp),
+    "min_eqm": np.min(eqms_mlp),
+    "mean_r2": np.mean(r_s_mlp),
+    "std_r2": np.std(r_s_mlp),
+    "max_r2": np.max(r_s_mlp),
+    "min_r2": np.min(r_s_mlp),
+}
 models = [
-    "Adaline"
+    "Adaline",
+    "MLP",
 ]
 
 metrics = {
     "Média EQM": 
         (
             metrics_adaline["mean_eqm"],
+            metrics_mlp["mean_eqm"],
         ),
     "Desvio Padrão EQM": 
         (
             metrics_adaline["max_eqm"],
+            metrics_mlp["max_eqm"],
         ),
     "Máximo EQM": 
         (
             metrics_adaline["max_eqm"],
+            metrics_mlp["max_eqm"],
         ),
     "Mínimo EQM": 
         (
             metrics_adaline["min_eqm"],
+            metrics_mlp["min_eqm"],
         ),
     "Média R²": 
         (
             metrics_adaline["mean_r2"],
+            metrics_mlp["mean_r2"],
         ),
     "Desvio Padrão R²": 
         (
             metrics_adaline["std_r2"],
+            metrics_mlp["std_r2"],
         ),
     "Máximo R²": 
         (
             metrics_adaline["max_r2"],
+            metrics_mlp["max_r2"],
         ),
     "Mínimo R²": 
         (
             metrics_adaline["min_r2"],
+            metrics_mlp["min_r2"],
         ),
 }
 
